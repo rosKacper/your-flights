d924521fbd8fe32968d85b46cbfd88bd95771806
Kacper Rosiak <ros.kacper@gmail.com>
2020-12-03 14:06:57 +0100

    Add TicketCategory and TicketOrder model

---
+++ src/main/java/pl/edu/agh/ki/lab/to/yourflights/model/ticketcategory/TicketCategory.java
@@ -0,0 +1,69 @@
+package pl.edu.agh.ki.lab.to.yourflights.model.ticketcategory;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+public class TicketCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    //TODO
+    //flightID
+
+    @NotEmpty
+    private String categoryName;
+
+    @NotNull
+    private BigDecimal categoryPrice;
+
+    @NotNull
+    private int totalNumberOfSeats;
+
+    public TicketCategory(){}
+
+
+    //TODO
+    //add flight to constructor
+    public TicketCategory(String categoryName, BigDecimal categoryPrice, int totalNumberOfSeats){
+        this.categoryName = categoryName;
+        this.categoryPrice = categoryPrice;
+        this.totalNumberOfSeats = totalNumberOfSeats;
+    }
+
+
+    //getters and setters
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public BigDecimal getCategoryPrice() {
+        return categoryPrice;
+    }
+
+    public void setCategoryPrice(BigDecimal categoryPrice) {
+        this.categoryPrice = categoryPrice;
+    }
+
+    public int getTotalNumberOfSeats() {
+        return totalNumberOfSeats;
+    }
+
+    public void setTotalNumberOfSeats(int totalNumberOfSeats) {
+        this.totalNumberOfSeats = totalNumberOfSeats;
+    }
+}
---
+++ src/main/java/pl/edu/agh/ki/lab/to/yourflights/model/ticketorder/TicketOrder.java
@@ -0,0 +1,48 @@
+package pl.edu.agh.ki.lab.to.yourflights.model.ticketorder;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Entity
+public class TicketOrder {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+
+    @NotNull
+    private int numberOfSeats;
+
+    //TODO
+    //reservationID, ticketCategoryID, ticketDiscountID
+    //used in mapping relations in database
+
+
+    public TicketOrder(){}
+
+
+    //TODO
+    //add fields used in mapping relations to constructor
+    public TicketOrder(int numberOfSeats){
+        this.numberOfSeats = numberOfSeats;
+    }
+
+
+
+    //getters and setters
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+}
